83c19df422883120c154fb80dd018905ae2a8a3b
eliezer <elifred21@hotmail.com>
2022-11-08 16:02:28 -0300

    quede en clase share aun no me carga la imagen

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+//
+import image from "./assets/logo.jpg"; // asi se llaman las imgaenes locales
+//import React, { useState } from "react";
 
-export default function App() {
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Pemission to access is required");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+    //console.log(pickerResult);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Pick an image¡¡</Text>
+      <TouchableOpacity onPress={openImagePickerAsync}>
+        <Image
+          //source={{ uri: "https://picsum.photos/200/200" }} de esta manera carho imagen de la web
+          //source={image} asi carga imagen local
+          source={{
+            uri:
+              selectedImage !== null
+                ? selectedImage.localuri
+                : "https://picsum.photos/200/200",
+          }}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Ingresa</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: `#292929`,
   },
+  title: { fontSize: 30, color: "#fff" },
+  image: { height: 200, width: 200, borderRadius: 100, resizeMode: "contain" },
+  button: { backgroundColor: "#dc143c", padding: 7, marginTop: 10 },
+  buttonText: { color: "#fff", fontSize: 20 },
 });
+
+export default App;
