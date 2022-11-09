e644acd14a2e5f754b4dca952630e9ad0ad3f204
eliezer <elifred21@hotmail.com>
2022-11-08 21:53:39 -0300

    hasta web

--- App.js
+++ App.js
@@ -5,55 +5,84 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
-  Alert,
+  Platform,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-//
-import image from "./assets/logo.jpg"; // asi se llaman las imgaenes locales
-//import React, { useState } from "react";
+import * as Sharing from "expo-sharing";
+import uploadAnonymousFilesAsync from "anonymous-files";
 
 const App = () => {
   const [selectedImage, setSelectedImage] = useState(null);
 
   let openImagePickerAsync = async () => {
-    let permissionResult =
-      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
 
     if (permissionResult.granted === false) {
-      alert("Pemission to access is required");
+      alert("Permission to camara roll is required");
       return;
     }
 
     const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    // console.log(pickerResult)
 
     if (pickerResult.cancelled === true) {
       return;
     }
 
-    setSelectedImage({ localUri: pickerResult.uri });
-    //console.log(pickerResult);
+    if (Platform.OS === "web") {
+      let remoteUri = await uploadAnonymousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri });
+    }
   };
+
+  let openShareDialog = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      // alert("Sharing, is not available on your platform");
+      alert(
+        `The image share is available for sharing at: ${selectedImage.remoteUri}`
+      );
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+
+  // if (selectedImage !== null) {
+  //   return (
+  //     <View styles={styles.container}>
+  //       <Image
+  //         source={{ uri: selectedImage.localUri }}
+  //         style={styles.thumbnail}
+  //       />
+  //     </View>
+  //   );
+  // }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Pick an image¡¡</Text>
+      <Text style={{ color: "#eee", fontSize: 20, marginBottom: 10 }}>
+        Change this image
+      </Text>
       <TouchableOpacity onPress={openImagePickerAsync}>
         <Image
-          //source={{ uri: "https://picsum.photos/200/200" }} de esta manera carho imagen de la web
-          //source={image} asi carga imagen local
           source={{
             uri:
               selectedImage !== null
-                ? selectedImage.localuri
+                ? selectedImage.localUri
                 : "https://picsum.photos/200/200",
           }}
           style={styles.image}
         />
       </TouchableOpacity>
-
-      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-        <Text style={styles.buttonText}>Ingresa</Text>
-      </TouchableOpacity>
+      {selectedImage ? (
+        <TouchableOpacity onPress={openShareDialog} style={styles.button}>
+          <Text style={styles.buttonText}>Share this Image</Text>
+        </TouchableOpacity>
+      ) : (
+        <View></View>
+      )}
     </View>
   );
 };
@@ -61,14 +90,23 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#292929",
     alignItems: "center",
-    backgroundColor: `#292929`,
+    justifyContent: "center",
   },
-  title: { fontSize: 30, color: "#fff" },
-  image: { height: 200, width: 200, borderRadius: 100, resizeMode: "contain" },
-  button: { backgroundColor: "#dc143c", padding: 7, marginTop: 10 },
-  buttonText: { color: "#fff", fontSize: 20 },
+  image: {
+    height: 100,
+    width: 100,
+    resizeMode: "contain",
+    // borderRadius: 100,
+  },
+  button: { backgroundColor: "#BB86FC", padding: 7, marginTop: 10 },
+  buttonText: { fontSize: 20, color: "#fff" },
+  thumbnail: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
 });
 
 export default App;
